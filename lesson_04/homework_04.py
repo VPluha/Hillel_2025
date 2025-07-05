a86a021c316adf5d12efaffbb87d11900f16a393
Vasyl Pliukhin <vasyapluhin@gmail.com>
2025-07-05 12:20:54 +0300

    Change_String

--- lesson_04/homework_04.py
+++ lesson_04/homework_04.py
@@ -27,39 +27,20 @@ rolling in wealth."""
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
 
-adwentures_of_tom_sawer = """\
-Tom gave up the brush with reluctance in his .... face but alacrity
-in his heart. And while
-the late steamer
-"Big Missouri" worked ....
-and sweated
-in the sun,
-the retired artist sat on a barrel in the .... shade close by, dangled his legs,
-munched his apple, and planned the slaughter of more innocents.
-There was no lack of material;
-boys happened along every little while;
-they came to jeer, but .... remained to whitewash. ....
-By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for
-a kite, in good repair;
-and when he played
-out, Johnny Miller bought
-in for a dead rat and a string to swing it with—and so on, and so on,
-hour after hour. And when the middle of the afternoon came, from being a
-poor poverty, stricken boy in the .... morning, Tom was literally
-rolling in wealth."""
-print(adwentures_of_tom_sawer.replace("\n", " "))
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("\n", " ")
+print(adwentures_of_tom_sawer)
 
 # task 02 ==
 """ Замініть .... на пробіл
 """
-adwentures_of_tom_sawer = """Tom gave up the brush with reluctance in his .... face but alacrity in his heart. And while the late steamer "Big Missouri" worked .... and sweated in the sun, the retired artist sat on a barrel in the .... shade close by, dangled his legs, munched his apple, and planned the slaughter of more innocents. There was no lack of material; boys happened along every little while; they came to jeer, but .... remained to whitewash. .... By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for a kite, in good repair; and when he played out, Johnny Miller bought in for a dead rat and a string to swing it with—and so on, and so on, hour after hour. And when the middle of the afternoon came, from being a poor poverty, stricken boy in the .... morning, Tom was literally rolling in wealth."""
-
-print(adwentures_of_tom_sawer.replace("....", " "))
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace("....", " ")
+print(adwentures_of_tom_sawer)
 # task 03 ==
 """ Зробіть так, щоб у тексті було не більше одного пробілу між словами.
 """
-adwentures_of_tom_sawer = """Tom gave up the brush with reluctance in his   face but alacrity in his heart. And while the late steamer "Big Missouri" worked   and sweated in the sun, the retired artist sat on a barrel in the   shade close by, dangled his legs, munched his apple, and planned the slaughter of more innocents. There was no lack of material; boys happened along every little while; they came to jeer, but   remained to whitewash.   By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for a kite, in good repair; and when he played out, Johnny Miller bought in for a dead rat and a string to swing it with—and so on, and so on, hour after hour. And when the middle of the afternoon came, from being a poor poverty, stricken boy in the   morning, Tom was literally rolling in wealth. """
-print(adwentures_of_tom_sawer.replace("   ", " "))
+adwentures_of_tom_sawer = adwentures_of_tom_sawer.split()
+adwentures_of_tom_sawer = " ".join(adwentures_of_tom_sawer)
+print(adwentures_of_tom_sawer)
 
 # task 04
 """ Виведіть, скількі разів у тексті зустрічається літера "h"
@@ -71,7 +52,6 @@ print(f"There is {adwentures_of_tom_sawer} h in sentences.")
 """ Виведіть, скільки слів у тексті починається з Великої літери?
 """
 adwentures_of_tom_sawer = """Tom gave up the brush with reluctance in his face but alacrity in his heart. And while the late steamer "Big Missouri" worked and sweated in the sun, the retired artist sat on a barrel in the shade close by, dangled his legs, munched his apple, and planned the slaughter of more innocents. There was no lack of material; boys happened along every little while; they came to jeer, but remained to whitewash. By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for a kite, in good repair; and when he played out, Johnny Miller bought in for a dead rat and a string to swing it with—and so on, and so on, hour after hour. And when the middle of the afternoon came, from being a poor poverty, stricken boy in the morning, Tom was literally rolling in wealth."""
-word = 'A'
 counter = 0
 for word in adwentures_of_tom_sawer:
     if word.isupper():
@@ -82,7 +62,8 @@ print(f"There is {counter} words in sentenсes")
 """ Виведіть позицію, на якій слово Tom зустрічається вдруге
 """
 adwentures_of_tom_sawer = """Tom gave up the brush with reluctance in his face but alacrity in his heart. And while the late steamer "Big Missouri" worked and sweated in the sun, the retired artist sat on a barrel in the shade close by, dangled his legs, munched his apple, and planned the slaughter of more innocents. There was no lack of material; boys happened along every little while; they came to jeer, but remained to whitewash. By the time Ben was fagged out, Tom had traded the next chance to Billy Fisher for a kite, in good repair; and when he played out, Johnny Miller bought in for a dead rat and a string to swing it with—and so on, and so on, hour after hour. And when the middle of the afternoon came, from being a poor poverty, stricken boy in the morning, Tom was literally rolling in wealth."""
-print(adwentures_of_tom_sawer.find("Tom", 1))
+index = adwentures_of_tom_sawer.index("Tom",2)
+print(f"The second word 'Tom' is in the {index}-th position.")
 
 # task 07
 """ Розділіть змінну adwentures_of_tom_sawer по кінцю речення.
